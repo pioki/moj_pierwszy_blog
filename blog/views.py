23660b3166e831ec65c369b95666c154830448e9
mikiop <ilion@o2.pl>
2023-05-02 09:49:43 +0200

    Modyfikacja szablonow do wyswietlenia wpisow z bazy danych.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Wpis
 
 # Create your views here.
 
 def lista_wpisow(request):
-  return render(request, 'blog/lista_wpisow.html', {})
+  wpisy = Wpis.objects.filter(data_opublikowania__lte=timezone.now()).order_by('data_opublikowania')
+  return render(request, 'blog/lista_wpisow.html', {'wpisy': wpisy})
