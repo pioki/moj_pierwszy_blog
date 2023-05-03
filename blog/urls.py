c3b3c0dc8f8abd88c98a8e8734f7cf7a939b85dc
mikiop <ilion@o2.pl>
2023-05-03 08:59:53 +0200

    Dodanie widoku i szablonu szczegolowego wpisu oraz CSS dla strony

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
   path('',views.lista_wpisow, name='lista_wpisow'),
-}
+  path('wpis/<int:pk>/',views.szczegoly_wpisu, name='szczegoly_wpisu'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Wpis
 
@@ -7,3 +7,7 @@ from .models import Wpis
 def lista_wpisow(request):
   wpisy = Wpis.objects.filter(data_opublikowania__lte=timezone.now()).order_by('data_opublikowania')
   return render(request, 'blog/lista_wpisow.html', {'wpisy': wpisy})
+
+def szczegoly_wpisu(request,pk):
+  wpis = get_object_or_404(Wpis, pk=pk)
+  return render(request, 'blog/szczegoly_wpisu.html', {'wpis': wpis})
